f743cfcc8333c969e12fe0578bef2ab0bb2364c2
Ryan See <ryanjosephsee@gmail.com>
2022-11-22 16:19:46 -0800

    commit tree a1ae663ef17fa71be015c616d89b825238455169

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
+  const anchorLinks = links.map(link => {
+    <a key= {link} href={link}>{link}</a>
+  })
+  console.log(anchorLinks);
   return <nav>{/* display an <a> tag for each link here */}</nav>;
 }
 
